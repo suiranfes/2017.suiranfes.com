a9c11e26a58d1c35eb0be5f07e5ef19fb9890212
tamaina <tamaina@hotmail.co.jp>
2017-02-14 23:29:45 +0900

    わーい！ 古いAndroidBrowser排除機構を改良したよ！

--- cache/main.js
+++ cache/main.js
@@ -15548,15 +15548,30 @@ var Popover = function (jQuery) {
 
 }();
 
-var isAndroidBrowser = false
-var ua = window.navigator.userAgent
-if (/Android/.test(ua) && /Linux; U;/.test(ua) && !/Chrome/.test(ua)) {
-  isAndroidBrowser = true
+function suiranfes_chkandro(){
+
+  var isOldAndroidBrowser = false;
+  var ua = window.navigator.userAgent;
+  if (/Android/.test(ua) && /Linux; U;/.test(ua) && !/Chrome/.test(ua)) {
+    isOldAndroidBrowser = true;
+  }
+
+  return isOldAndroidBrowser
+
 }
+
 $(function(){
-  if(isAndroidBrowser == true){
+
+  if(suiranfes_chkandro() == true){
     $('.loading').html('<p><a href="intent://suiranfes.com#Intent;scheme=https;action=android.intent.action.VIEW;end">タップでブラウザアプリを開きます。</a></p>');
-    return false;
+    throw new Error('古いアンドロイドの埋め込みブラウザを検出しました。以降のDOMツリー構築後に行う処理を中止します。');
+  }
+
+});
+
+$(window).on('load',function(){
+  if(suiranfes_chkandro() == true){
+    throw new Error('古いアンドロイドの埋め込みブラウザを検出しました。以降のデータ読み込み終了後に行う処理を中止します。');
   }
 });
 $(window).on('load',function(){
