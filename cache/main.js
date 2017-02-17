13e5cca8926390a7580301aafb5ec306d358707b
tamaina <tamaina@hotmail.co.jp>
2017-02-17 16:06:30 +0900

    ✌

--- cache/main.js
+++ cache/main.js
@@ -15576,6 +15576,14 @@ $(window).on('load',function(){
   }
 
 });
+$(function(){
+setTimeout(function() {
+    $(".loading").fadeOut("fast");
+    $(".wrap").css("display","block");
+    $(".fontloader").css("display","none");
+}, 10000);
+});
+
 $(window).on('load',function(){
     $(".loading").fadeOut("fast");
     $(".wrap").css("display","block");
--- src/js/loading.js
+++ src/js/loading.js
@@ -1,3 +1,11 @@
+$(function(){
+setTimeout(function() {
+    $(".loading").fadeOut("fast");
+    $(".wrap").css("display","block");
+    $(".fontloader").css("display","none");
+}, 10000);
+});
+
 $(window).on('load',function(){
     $(".loading").fadeOut("fast");
     $(".wrap").css("display","block");
