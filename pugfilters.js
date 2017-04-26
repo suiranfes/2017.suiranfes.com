53332fb55b56721d1076122d2c8d203144e0141a
tamaina <tamaina@hotmail.co.jp>
2017-04-26 23:45:03 +0900

    まだ終わっていない

--- pugfilters.js
+++ pugfilters.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var marked = require('marked');
+var kramed = require('kramed');
 var UglifyJS = require('uglify-js');
 var stylus = require('stylus');
 var pugfilters = module.exports = {};
@@ -7,11 +8,20 @@ var pugfilters = module.exports = {};
 pugfilters.md = function(str) {
     return marked(str).replace(/\r?\n/g,"");
 };
+pugfilters.kd = function(str) {
+    return kramed(str).replace(/\r?\n/g,"");
+};
 pugfilters.markdown = function(str) {
     return marked(str).replace(/\r?\n/g,"");
 };
+pugfilters.kramdown = function(str) {
+    return kramed(str).replace(/\r?\n/g,"");
+};
 pugfilters.marked = function(str) {
     return marked(str).replace(/\r?\n/g,"");
+};
+pugfilters.kramed = function(str) {
+    return kramed(str).replace(/\r?\n/g,"");
 };
 pugfilters.UglifyJS = function(str) {
     var result = UglifyJS.minify(str, {fromString: true}).code;
