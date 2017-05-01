acbd62b3a7a62a4a37ec086b703986eeca95d3f3
tamaina <tamaina@hotmail.co.jp>
2017-05-02 02:05:44 +0900

    完全なるPWAに昇華した

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -1,7 +1,7 @@
 var OneSignal = window.OneSignal || [];
 OneSignal.push(["init", {
   appId: "caa81f3a-0264-4b5f-916b-ff5484aa6a2b",
-  autoRegister: true,
+  autoRegister: false,
   promptOptions: {
         /* These prompt options values configure both the HTTP prompt and the HTTP popup. */
         /* actionMessage limited to 90 characters */
@@ -10,6 +10,11 @@ OneSignal.push(["init", {
         acceptButtonText: "通知を受け取る",
         /* cancelButtonText limited to 15 characters */
         cancelButtonText: "受け取らない"
+  },
+  welcomeNotification: {
+        "title": "第65回翠巒祭",
+        "message": "サイトからの通知が許可されました。",
+        // "url": "" /* Leave commented for the notification to not open a window on Chrome and Firefox (on Safari, it opens to your webpage) */
   },
   safari_web_id: 'web.onesignal.auto.14c30330-c4fb-483f-9c66-942ad147f917'
 }]);
