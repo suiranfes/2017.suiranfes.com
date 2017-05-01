00b45c92d5a81221071a797acdd9c7dd4921b824
tamaina <tamaina@hotmail.co.jp>
2017-05-02 02:46:08 +0900

    1.5.10

--- docs/sfesWorker.js
+++ docs/sfesWorker.js
@@ -59,7 +59,7 @@ var cachepages = [
     "/manifest.json",
 
 ];
-var version = '1.5.9';
+var version = '1.5.11';
 this.addEventListener('install', function(event) {
   event.waitUntil(self.skipWaiting());
   event.waitUntil(
